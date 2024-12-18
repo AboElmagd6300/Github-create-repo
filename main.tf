66d610fa7dc357da14f891f625907aee60d3e545
Magdi <m.magdi.hafez@gmail.com>
2024-12-18 19:24:54 +0300

    pull from github to push again with updates

--- main.tf
+++ main.tf
@@ -23,6 +23,13 @@ resource "github_repository" "github-create-repo" {
 }
 
 // module to pull repo
+/*
 module "github_repo" {
   source = "github.com/AboElmagd6300/DolfinedProject-repo.git"
+}
+*/
+
+// module to pull branch
+module "github_repo" {
+  source = "github.com/AboElmagd6300/Github-create-repo.git?ref=Branchpull"
 }
